5918016edab9c4e6f3376e34f995573cab2c1573
Phong <phongnguyenwm3@gmail.com>
2024-04-11 22:00:40 +0700

    update signup

--- src/services/userManagement.js
+++ src/services/userManagement.js
@@ -4,4 +4,7 @@ export const userManagementServ = {
   logIn: (data) => {
     return http.post("/QuanLyNguoiDung/DangNhap", data);
   },
+  signUp: (data) => {
+    return http.post("/QuanLyNguoiDung/DangKy", data);
+  },
 };
