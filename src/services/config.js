bfcbe16580ec0eee0d298ed8cec4543e53089add
VSOP40 <phongnguyenwm3@gmail.com>
2024-10-07 12:11:55 +0700

    update token

--- src/services/config.js
+++ src/services/config.js
@@ -3,7 +3,7 @@ export const http = axios.create({
   baseURL: "https://elearningnew.cybersoft.edu.vn/api",
   headers: {
     tokenCyberSoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTXHUwMGUxbmcgMTAiLCJIZXRIYW5TdHJpbmciOiIwMS8wOS8yMDI0IiwiSGV0SGFuVGltZSI6IjE3MjUxNDg4MDAwMDAiLCJuYmYiOjE2OTg2ODUyMDAsImV4cCI6MTc2NzIyNTU5OX0.0dUDEMt1iBLA2haTnrKqKPLKPz2x4MAmVZlxXq-ff54",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTXHUwMGUxbmcgMTAiLCJIZXRIYW5TdHJpbmciOiIzMS8xMi8yMDI1IiwiSGV0SGFuVGltZSI6MTc2NzIyNTU5OTAwMCwibmJmIjoxNjk4Njg1MjAwLCJleHAiOjE3NjcyMjU1OTl9.loRPS1RZwfJ0WEe5S9bQER2C0PiRLuJ-8r3CREErSLs",
   },
   timeout: 30000,
 });
