646f3320d798ddc77c6c1340c6c7f336f78fb6a4
VSOP40 <phongnguyenwm3@gmail.com>
2024-10-08 19:32:21 +0700

    test new baseURL

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const http = axios.create({
-  baseURL: "https://elearningnew.cybersoft.edu.vn/swagger/index.html",
+  baseURL: "https://elearningnew.cybersoft.edu.vn/swagger/index.html/api",
   headers: {
     tokenCyberSoft:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTXHUwMGUxbmcgMTAiLCJIZXRIYW5TdHJpbmciOiIzMS8xMi8yMDI1IiwiSGV0SGFuVGltZSI6MTc2NzIyNTU5OTAwMCwibmJmIjoxNjk4Njg1MjAwLCJleHAiOjE3NjcyMjU1OTl9.loRPS1RZwfJ0WEe5S9bQER2C0PiRLuJ-8r3CREErSLs",
